4ba1364b33e412512782cf6c10a0baa4ecafa848
Alison Law <alisparklaw@gmail.com>
2019-11-08 10:07:06 +0000

    mapped the array.  axios data populating cards

--- starwars/src/components/starwarsData.js
+++ starwars/src/components/starwarsData.js
@@ -11,7 +11,8 @@ function StarwarsData() {
       .get("https://swapi.co/api/people/")
       .then(response => {
         console.log("Charachters: ", response.data);
-        fetchStarwarsChars(response.data);
+        console.log("Charachters: ", response.data.results);
+        fetchStarwarsChars(response.data.results);
       })
       .catch(error => {
         console.log("the data was not returned", error);
@@ -19,11 +20,32 @@ function StarwarsData() {
   }, []);
 
   return (
-    <div>
-       
-      <Card />
+     <div className ="CardContainer">
+       {
+        starwarsChars.map(character => {
+            return(
+               <Card 
+                
+                name={character.name} 
+                height={character.height}
+                mass={character.mass} 
+                hair_color={character.hair_color} 
+                skin_color={character.skin_color} 
+                
+                />
+            )
+        })}
+       </div>
+
+    
+    //   <h3>Name: {props.name}</h3>
+    //   <h3>Height: {props.height}</h3>
+    //   <h3>Mass: {props.mass}</h3>
+    //   <h3>Hair colour: {props.hair_color}</h3>
+    //   <h3>Skin colour: {props.skin_color}</h3>
       
-    </div>
+      
+    
   );
 }
 export default StarwarsData;
