250969d50991bcd5032732408d963799b2a31697
Alison Law <alisparklaw@gmail.com>
2019-11-08 10:45:19 +0000

    further styling

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -5,17 +5,20 @@ import styled from "styled-components";
 const StyledDiv = styled.div`
 	margin: 20px auto;
 	width: 60%;
-	border: purple 2px, solid;
+    border: rgb(210, 180, 140) 2px solid;
 	height: auto;
 	border-radius: 4px;
 	box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
     overflow: hidden;
+    &:hover {
+    background-color: rgb(210, 180, 140);
+		}
     `
 
 const Card = props => {
   return (
     <StyledDiv>
-      <h3>Name: {props.name}</h3>
+      <h1>{props.name}</h1>
       <h3>Height: {props.height}</h3>
       <h3>Mass: {props.mass}</h3>
       <h3>Hair colour: {props.hair_color}</h3>
--- starwars/src/components/starwarsData.js
+++ starwars/src/components/starwarsData.js
@@ -5,12 +5,13 @@ import styled from "styled-components";
 
 // styling
 const DivStyled = styled.div`
-
-  border: rgb(240, 192, 246) 1px solid;
+   
   height: auto;
   border-radius: 4px;
-  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.5), 0 15px 12px rgba(0, 0, 0, 0.22);
   overflow: hidden;
+  
+  
 `;
 
 function StarwarsData() {
@@ -46,12 +47,6 @@ function StarwarsData() {
       })}
          
     </DivStyled>
-
-    //   <h3>Name: {props.name}</h3>
-    //   <h3>Height: {props.height}</h3>
-    //   <h3>Mass: {props.mass}</h3>
-    //   <h3>Hair colour: {props.hair_color}</h3>
-    //   <h3>Skin colour: {props.skin_color}</h3>
   );
 }
 export default StarwarsData;
